051faf34fecc3633c8d92e453c7419a2fb39cf01
luciam9 <luciamontiel@uma.es>
2020-11-29 09:55:32 +0100

    fyju

---
+++ ADA/Práctica2/src/SubArrayMaximoNLOGN.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author ***** Indicar aqui el autor de la practica *******
+ *
+ */
+public class SubArrayMaximoNLOGN extends SubArrayMaximo {
+
+	public SolucionSubArrayMaximo resolver(int[] array) {
+		SolucionSubArrayMaximo ssam = new SolucionSubArrayMaximo();
+		 /** Implementar aqui una solucion mediante el metodo Divide y Venceras */
+		return ssam;
+	}
+	
+}
